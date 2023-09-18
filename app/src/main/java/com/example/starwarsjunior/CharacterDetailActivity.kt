127910ee50a89d9a3831373f006bea81b382a2e3
JorgeBarrosLobo <144116251+JorgeBarrosLobo@users.noreply.github.com>
2023-09-18 11:20:45 +0100

    Update MainActivity, lists and details

--- app/src/main/java/com/example/starwarsjunior/CharacterDetailActivity.kt
+++ app/src/main/java/com/example/starwarsjunior/CharacterDetailActivity.kt
@@ -2,10 +2,18 @@ package com.example.starwarsjunior
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.starwarsjunior.databinding.ActivityCharacterDetailBinding
 
 class CharacterDetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCharacterDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_character_detail)
+        binding = ActivityCharacterDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.backButton.setOnClickListener {
+            finish()
+        }
+
     }
 }
--- app/src/main/java/com/example/starwarsjunior/MainActivity.kt
+++ app/src/main/java/com/example/starwarsjunior/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.starwarsjunior
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.starwarsjunior.databinding.ActivityMainBinding
 
@@ -15,17 +14,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.switchModeButton.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                binding.imageLightMode.visibility = ImageView.VISIBLE
-                binding.imageDarkMode.visibility = ImageView.INVISIBLE
-
-            } else {
-                binding.imageLightMode.visibility = ImageView.INVISIBLE
-                binding.imageDarkMode.visibility = ImageView.VISIBLE
-            }
-        }
 
         binding.charactersButton.setOnClickListener {
             startActivity(Intent(this, CharactersListActivity::class.java))

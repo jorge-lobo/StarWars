12439c8dbcd36e10b849217d076ed875111dc486
JorgeBarrosLobo <144116251+JorgeBarrosLobo@users.noreply.github.com>
2023-09-25 16:07:00 +0100

    Update applicationId

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.starwarsjunior"
+        applicationId = "com.axians.starshutai"
         minSdk = 24
         targetSdk = 33
         versionCode = 1

943821f41fc88f540a7e575b0fa0e60546fd1b82
ed-ribeiro <eduardo.frutuoso-ribeiro@axians.com>
2023-09-25 16:31:47 +0100

    - added project to firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -8,7 +9,7 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.example.starwarsjunior"
+        applicationId = "com.axians.starshutai"
         minSdk = 24
         targetSdk = 33
         versionCode = 1
@@ -52,4 +53,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
 }

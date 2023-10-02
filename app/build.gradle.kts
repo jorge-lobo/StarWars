4af219cae7421dcfda62f9fb2957a80fff26b4ef
JorgeBarrosLobo <144116251+JorgeBarrosLobo@users.noreply.github.com>
2023-10-02 12:56:00 +0100

    - Update versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.axians.starshutai"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
-        versionName = "0.2"
+        versionCode = 2
+        versionName = "0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

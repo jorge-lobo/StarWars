25b3be49f00e6072424680cd8aae8295c3a09209
JorgeBarrosLobo <144116251+JorgeBarrosLobo@users.noreply.github.com>
2023-10-18 10:07:05 +0100

    - feat: add ship details

--- app/src/main/java/com/example/starwarsjunior/ui/ship/details/ShipDetailViewModel.kt
+++ app/src/main/java/com/example/starwarsjunior/ui/ship/details/ShipDetailViewModel.kt
@@ -9,12 +9,24 @@ import com.example.starwarsjunior.data.ship.ShipRepository
 import com.example.starwarsjunior.data.ship.objects.Ship
 import com.example.starwarsjunior.ui.common.BaseViewModel
 import kotlinx.coroutines.launch
+import java.text.DecimalFormat
 
 class ShipDetailViewModel(application: Application) : BaseViewModel(application),
     LifecycleObserver {
 
     var shipName = MutableLiveData<String>().apply { value = "" }
-
+    var shipModel = MutableLiveData<String>().apply { value = "" }
+    var shipManufacturer = MutableLiveData<String>().apply { value = "" }
+    var shipCostInCredits = MutableLiveData<String>().apply { value = "" }
+    var shipLength = MutableLiveData<String>().apply { value = "" }
+    var shipMaxAthmospheringSpeed = MutableLiveData<String>().apply { value = "" }
+    var shipCrew = MutableLiveData<String>().apply { value = "" }
+    var shipPassengers = MutableLiveData<String>().apply { value = "" }
+    var shipCargoCapacity = MutableLiveData<String>().apply { value = "" }
+    var shipConsumables = MutableLiveData<String>().apply { value = "" }
+    var shipHyperdriveRating = MutableLiveData<String>().apply { value = "" }
+    var shipMglt = MutableLiveData<String>().apply { value = "" }
+    var shipStarshipClass = MutableLiveData<String>().apply { value = "" }
 
     fun initialize(shipId: Int) {
         getShipDetails(shipId)
@@ -31,7 +43,18 @@ class ShipDetailViewModel(application: Application) : BaseViewModel(application)
                     override fun onSuccess(data: Ship?) {
                         if (data != null) {
                             shipName.value = data.name.lowercase()
-
+                            shipModel.value = data.model.lowercase()
+                            shipManufacturer.value = data.manufacturer.lowercase()
+                            shipCostInCredits.value = formatter(data.costInCredits.lowercase())
+                            shipLength.value = data.length.lowercase()
+                            shipMaxAthmospheringSpeed.value = data.maxAtmospheringSpeed.lowercase()
+                            shipCrew.value = formatter(data.crew.lowercase())
+                            shipPassengers.value = formatter(data.passengers.lowercase())
+                            shipCargoCapacity.value = formatter(data.cargoCapacity.lowercase())
+                            shipConsumables.value = data.consumables.lowercase()
+                            shipHyperdriveRating.value = data.hyperdriveRating.lowercase()
+                            shipMglt.value = data.mGLT.lowercase()
+                            shipStarshipClass.value = data.starshipClass.lowercase()
 
                         } else {
                             onError("Unknown error")
@@ -41,6 +64,11 @@ class ShipDetailViewModel(application: Application) : BaseViewModel(application)
                 }
         }
     }
+
+    fun formatter(n: String): String? = runCatching {
+        val numberSeparator = n.replace(",", "").toInt()
+        DecimalFormat("#,###").format(numberSeparator)
+    }.getOrElse { n }
 
     override fun onError(message: String?, validationErrors: Map<String, ArrayList<String>>?) {
         isLoading.value = false
